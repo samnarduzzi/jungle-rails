4f86570a0d2328ddbe03d0ebd61353f7722465f6
Sam <sammynarduzzi@gmail.com>
2023-03-08 19:58:47 -0800

    create admin dashboard with couts and links to other admin pages

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USER_NAME'], password: ENV['PASSWORD']
   def show
+    @products_count = Product.sum(:quantity)
+    @category_count = Category.count
   end
 end
