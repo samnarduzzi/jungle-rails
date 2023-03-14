eb3f99caef1283c55f06a67837db303f9f529120
Sam <sammynarduzzi@gmail.com>
2023-03-14 13:15:43 -0700

    home page tests using cypress on rails

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,13 @@
+describe("Home Page", () => {
+  it("should visit the home page", () => {
+    cy.visit("http://localhost:3000/");
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 12 products on the page", () => {
+    cy.get(".products article").should("have.length", 12);
+  });
+});
